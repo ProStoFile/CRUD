10828f18c71b94e94453b687b38a8e1a2acbbad0
Aleksey Andreev <fistashki22@gmail.com>
2022-12-12 22:42:40 +0300

    add db config

---
+++ crud-app/config/db.js
@@ -0,0 +1,24 @@
+var mysql = require('mysql')
+var connection = ({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'node_js_crud'
+})
+var db;
+function connectDb() {
+    if (!db) {
+        db = mysql.createConnection(connection);
+        db.connect(function (err) {
+            if (!err) {
+                console.log("database connected success!!");
+                console.log(connection);
+            } else {
+                console.log("Error database connect!!");
+            }
+        });
+    }
+    return db;
+}
+
+module.exports = connectDb;
--- crud-app/routes/index.js
+++ crud-app/routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'node js express crud app' });
 });
 
+router.post('/saveFomData', function (req, res) {
+  const userData = {
+    fullname: req.body.txt_full_name,
+    email: req.body.txt_email,
+    contact: req.body.txt_contact,
+    description: req.body.txt_description,
+    isactive: 1,
+  };
+  console.log(userData);
+  
+});
 module.exports = router;
