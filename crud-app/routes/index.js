2483e571bbeba32b3b4a77bd9f7e508860f993a3
Aleksey Andreev <fistashki22@gmail.com>
2022-12-13 01:26:48 +0300

    connect to db

--- crud-app/routes/index.js
+++ crud-app/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../config/db');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -15,6 +16,18 @@ router.post('/saveFomData', function (req, res) {
     isactive: 1,
   };
   console.log(userData);
-  
+  connection.query("INSERT INTO tbl_node_crud SET?", userData, function (err, result) {
+    if (err) {
+      console.log(err, '{"message" : "internal error.!", "status" : 500}');
+      res.end('{"message" : "internal error.!", "status" : 500}');
+    } else {
+      console.log('record successfully save');
+      console.log(userData);
+      res.end('{"message" : "Record created successfully...", "status" : 200}');
+    }
+  });
+
+
+
 });
 module.exports = router;
